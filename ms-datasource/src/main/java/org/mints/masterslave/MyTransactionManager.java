484c906ed929069c4f06eeaaf67c92a637c6a6f4
mac <hackxboy1986@163.com>
2021-06-04 20:26:43 +0800

    修复因doBegin异常导致未恢复上下文的bug:TransactionSynchronizationManager#unbindResource异常

--- ms-datasource/src/main/java/org/mints/masterslave/MyTransactionManager.java
+++ ms-datasource/src/main/java/org/mints/masterslave/MyTransactionManager.java
@@ -1,17 +1,14 @@
 package org.mints.masterslave;
 
-import cn.hutool.core.collection.CollectionUtil;
 import org.mints.masterslave.logger.MsLogger;
 import org.mints.masterslave.strategy.DsStrategy;
 import org.springframework.lang.Nullable;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.CannotCreateTransactionException;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionException;
-import org.springframework.transaction.support.AbstractPlatformTransactionManager;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
-import org.springframework.transaction.support.TransactionSynchronizationUtils;
 
-import javax.persistence.EntityManagerFactory;
 import java.util.Map;
 
 import static org.mints.masterslave.strategy.DsStrategy.DsStrategyStage.AOP_TRANSACTION_STAGE;
@@ -22,22 +19,22 @@ public class MyTransactionManager extends JpaTransactionManager {
 
     DsStrategy dsStrategy;
 
-    public MyTransactionManager(DsStrategy dsStrategy){
+    public MyTransactionManager(DsStrategy dsStrategy) {
         this.dsStrategy = dsStrategy;
     }
 
 
-    void suspendIfNeed(Object transaction){
+    void suspendIfNeed(Object transaction) {
         Map<Object, Object> resourceMap = TransactionSynchronizationManager.getResourceMap();
         Object actualKey = obtainEntityManagerFactory();
 //        Object actualKey = TransactionSynchronizationUtils.unwrapResourceIfNecessary(this.obtainEntityManagerFactory());
-        if (!resourceMap.containsKey(actualKey)){
+        if (!resourceMap.containsKey(actualKey)) {
             return;
         }
         suspendedResources.set(super.doSuspend(transaction));
     }
 
-    void resumeIfNeed(Object transaction){
+    void resumeIfNeed(Object transaction) {
         if (suspendedResources.get() != null) {
             super.doResume(transaction, suspendedResources.get());
             suspendedResources.remove();
@@ -46,32 +43,37 @@ public class MyTransactionManager extends JpaTransactionManager {
 
     @Override
     protected void doBegin(Object transaction, TransactionDefinition definition) {
-        if (definition.isReadOnly()){
-            logger.info("[ms-ds][事务]开始-只读 {} {}" , definition,definition.getName());
-        }else {
-            logger.info("[ms-ds][事务]开始-非只读 {} {}" , definition,definition.getName());
+        if (definition.isReadOnly()) {
+            logger.info("[ms-ds][事务]开始-只读 {} {}", definition, definition.getName());
+        } else {
+            logger.info("[ms-ds][事务]开始-非只读 {} {}", definition, definition.getName());
         }
-        dsStrategy.doStrategy(AOP_TRANSACTION_STAGE, definition.isReadOnly(),definition.getName());
+        dsStrategy.doStrategy(AOP_TRANSACTION_STAGE, definition.isReadOnly(), definition.getName());
 
         suspendIfNeed(transaction);
-        super.doBegin(transaction, definition);
+        try {
+            super.doBegin(transaction, definition);
+        } catch (TransactionException e) {
+            /**
+             * 事务进行suspendIfNeed后如果dobegin异常而此时不resumeIfNeed进行恢复上下文，当此线程后续还有请求时，会出现资源不平衡
+             * {@link TransactionSynchronizationManager#unbindResource} 会抛出异常: No value for key [org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean@6de610c6] bound to thread
+             */
+            resumeIfNeed(transaction);
+            throw e;
+        }
     }
 
-//    @Override
-//    protected void resume(@Nullable Object transaction, @Nullable AbstractPlatformTransactionManager.SuspendedResourcesHolder resourcesHolder) throws TransactionException {
-//    }
-
     @Override
-    protected void doResume(@Nullable Object transaction, Object suspendedResources){
-        dsStrategy.clear(false,AOP_TRANSACTION_STAGE);
+    protected void doResume(@Nullable Object transaction, Object suspendedResources) {
+        dsStrategy.clear(false, AOP_TRANSACTION_STAGE);
         logger.info("[ms-ds][事务]结束-doResume");
-        super.doResume(transaction,suspendedResources);
+        super.doResume(transaction, suspendedResources);
         resumeIfNeed(transaction);
     }
 
     @Override
     protected void doCleanupAfterCompletion(Object transaction) {
-        dsStrategy.clear(false,AOP_TRANSACTION_STAGE);
+        dsStrategy.clear(false, AOP_TRANSACTION_STAGE);
         logger.info("[ms-ds][事务]结束-doCleanupAfterCompletion");
         super.doCleanupAfterCompletion(transaction);
         resumeIfNeed(transaction);
@@ -85,4 +87,11 @@ public class MyTransactionManager extends JpaTransactionManager {
 //            }
 //            logger.info("[事务]断开缓存");
 //        }
+
+    //    public static int testTrigger = 0;
+//    void testEx() {
+//      if(testTrigger!=0) {
+//          throw new CannotCreateTransactionException("Could not open JPA EntityManager for transaction");
+//      }
+//    }
 }
